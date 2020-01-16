eedeab355489735e9f5be18699a12bbabb6ef566
jingyu323 <xzg323@163.com>
2020-01-17 07:45:52 +0800

    测试每次修 需要输入密码

--- LeetCodeStudy/src/main/java/rain/test/study20200112/Dp02EditDistance.java
+++ LeetCodeStudy/src/main/java/rain/test/study20200112/Dp02EditDistance.java
@@ -1,5 +1,8 @@
 package rain.test.study20200112;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /***
  *编辑距离，又称Levenshtein距离（也叫做Edit Distance），
  * 是指两个字串之间，由一个转成另一个所需的最少编辑操作次数。
@@ -7,4 +10,19 @@ package rain.test.study20200112;
  */
 
 public class Dp02EditDistance {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String aStr = scan.nextLine();
+        String bStr = scan.nextLine();
+        int aLen = aStr.length();
+        int bLen = bStr.length();
+
+        int[][] dp = new int[aLen + 1][bLen + 1];
+        for (int i = 0; i < aLen + 1; i++) {
+            dp[i][0] = i;
+        }
+        System.out.println(Arrays.toString(dp));
+    }
+
+
 }
