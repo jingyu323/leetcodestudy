654248f651db7a28d7b2cca3f88fbdbb1e531617
jingyu323 <xzg323@163.com>
2020-01-17 08:18:22 +0800

    贪吃蛇

--- LeetCodeStudy/src/main/java/rain/test/study20201017/SnakeGame.java
+++ LeetCodeStudy/src/main/java/rain/test/study20201017/SnakeGame.java
@@ -1,5 +1,8 @@
 package rain.test.study20201017;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * 353. 贪吃蛇
  * 请你设计一个 贪吃蛇游戏，该游戏将会在一个 屏幕尺寸 = 宽度 x 高度 的屏幕上运行。如果你不熟悉这个游戏，可以 点击这里 在线试玩。
@@ -53,4 +56,86 @@ package rain.test.study20201017;
  * snake.move("U"); -> 函数返回 -1 (蛇与边界相撞，游戏结束)
  */
 public class SnakeGame {
+
+    int[][] food;
+    int width, height;
+    int i, j;
+    Deque<int[]> data;
+    boolean[][] flag;
+    private int foodIdx;
+
+    /**
+     * Initialize your data structure here.
+     *
+     * @param width  - screen width
+     * @param height - screen height
+     * @param food   - A list of food positions
+     *               E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0].
+     */
+    public SnakeGame(int width, int height, int[][] food) {
+        this.width = width;
+        this.height = height;
+        this.food = food;
+        foodIdx = 0;
+        i = 0;
+        j = 0;
+
+        data = new ArrayDeque<>();
+        data.push(new int[]{0, 0});
+
+        flag = new boolean[height][width];
+        flag[0][0] = true;
+
+
+    }
+
+    public static void main(String[] args) {
+//        SnakeGame obj = new SnakeGame(width, height, food);
+//        int param_1 = obj.move(direction);
+    }
+
+    private boolean isValid(int i, int j) {
+        return i >= 0 && i < height && j >= 0 && j < width;
+    }
+
+    /**
+     * Moves the snake.
+     *
+     * @param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down
+     * @return The game's score after the move. Return -1 if game over.
+     * Game over when snake crosses the screen boundary or bites its body.
+     */
+    public int move(String direction) {
+        int new_i = i, new_j = j;
+        if (direction.equals("U")) new_i--;
+        if (direction.equals("D")) new_i++;
+        if (direction.equals("L")) new_j--;
+        if (direction.equals("R")) new_j++;
+
+        //判断是否到边界
+        if (!isValid(new_i, new_j)) {
+            return -1;
+        }
+
+        int[] lastPos = data.getLast();
+        if (flag[new_i][new_j] && (new_i != lastPos[0] || new_j != lastPos[1])) {
+            return -1;
+        }
+        data.addFirst(new int[]{new_i, new_j});
+        flag[new_i][new_j] = true;
+        if (foodIdx < food.length && food[foodIdx][0] == new_i && food[foodIdx][1] == new_j) {
+            foodIdx++;
+        } else {
+            int[] tail = data.removeLast();
+            if (tail[0] != new_i || tail[1] != new_j)
+                flag[tail[0]][tail[1]] = false;
+        }
+
+        i = new_i;
+        j = new_j;
+
+        return data.size() - 1;
+
+
+    }
 }
